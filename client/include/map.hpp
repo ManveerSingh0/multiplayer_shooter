83e8f2068ec201edd3ef4ff7ba3a2232560d0b7d
ManveerSingh0 <singhcoder2005@gmail.com>
2026-07-12 11:49:49 +0530

    today i will add map in it

--- client/include/map.hpp
+++ client/include/map.hpp
@@ -1,2 +1,24 @@
 #pragma once
 
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transform.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Vertex.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+
+
+#include <filesystem>
+#include <cstdint>
+
+
+class Map : public sf::Drawable , public sf::Transform{
+
+public:
+  Map();
+  bool load(std::filesystem::path& tile_path, sf::Vector2u tile_size, const int* tiles,  uint32_t width, uint32_t height);
+
+private:
+  sf::Texture m_tile_set;
+  sf::VertexArray m_tile_verticies;
+
+};
--- client/src/map.cpp
+++ client/src/map.cpp
@@ -1,7 +1,18 @@
+#include <SFML/Graphics/PrimitiveType.hpp>
 #include <map.hpp>
 
 
+Map::Map() {}
 
-class Map{
-    
-};
+
+bool Map::load(std::filesystem::path &tile_path, sf::Vector2u tile_size,
+               const int *tiles, uint32_t width, uint32_t height) {
+
+  if(!m_tile_set.loadFromFile(tile_path)){
+    return false;
+  }
+
+  m_tile_verticies.setPrimitiveType(sf::PrimitiveType::Triangles);
+  m_tile_verticies.resize(width * height * 6);
+}
+
