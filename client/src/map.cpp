db89bf25f32ffca7df99eed0ad2516c3be9d7e57
ManveerSingh0 <singhcoder2005@gmail.com>
2026-07-15 23:40:30 +0530

    nothing

--- client/src/map.cpp
+++ client/src/map.cpp
@@ -8,11 +8,13 @@ Map::Map() {}
 bool Map::load(std::filesystem::path &tile_path, sf::Vector2u tile_size,
                const int *tiles, uint32_t width, uint32_t height) {
 
+  //Checks if the file 
   if(!m_tile_set.loadFromFile(tile_path)){
     return false;
   }
 
   m_tile_verticies.setPrimitiveType(sf::PrimitiveType::Triangles);
   m_tile_verticies.resize(width * height * 6);
+  
 }
 
